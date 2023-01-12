b17698e18cff8986e661f9d0ca1b476e6a9398e7
yee <pofey@me.com>
2023-01-12 15:25:29 +0800

    test mediaserver

---
+++ tests/test_mediaserver.py
@@ -0,0 +1,8 @@
+from mbot.external.mediaserver import PlexMediaServer
+
+plex = PlexMediaServer(url='http://192.168.1.50:32400', token='5ZeHqTLzjkyoZUPPSRYW')
+
+
+def test_get_episodes_from_tmdbid():
+    items = plex.get_episodes_from_tmdbid(60059, 1)
+    print(len(items))
